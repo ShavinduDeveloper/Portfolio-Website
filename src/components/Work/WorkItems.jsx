1888b3b80351de21a5a8e533a7607e0923dd5574
Shavindu Thushara <shavindu.thushara.2001.developer@gmail.com>
2024-03-11 19:07:49 +0530

    update

--- src/components/Work/WorkItems.jsx
+++ src/components/Work/WorkItems.jsx
@@ -5,7 +5,7 @@ const WorkItems = ({item}) => {
     <div className="work__card" key={item.id}>
         <img src={item.image} className="work__img" alt='item img'></img>
         <h3 className="work__title">{item.title}</h3>
-        <a href="http://www.example.com" className="work__button" target="_blank" rel="noreferrer">
+        <a href={item.url} className="work__button" target="_blank" rel="noreferrer">
             Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
     </div>
