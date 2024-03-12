2ae509f9517447fee0f6596d9e35f3b0a8bcad59
Shavindu Thushara <shavindu.thushara.2001.developer@gmail.com>
2024-03-13 03:23:40 +0530

    update

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -1,149 +1,196 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
+const Work1 = "https://github.com/ShavinduDeveloper/ECOSOL/blob/main/ECOSOL%20Introducing%20Post.png?raw=true";
+const Work2 = "https://github.com/ShavinduDeveloper/Sidebar/blob/main/Introducing%20Sidebar%20App%20Presentation.png?raw=true";
+const Work3 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
+const Work4 = "https://github.com/ShavinduDeveloper/Doughnut/blob/main/Views/Views%20of%20After%20Adding%20Texture%20and%20Colours/Left%20View.jpg?raw=true";
+const Work5 = "https://github.com/ShavinduDeveloper/7Up-Bottle/blob/main/Front%20View.jpg?raw=true";
+const Work6 = "https://github.com/ShavinduDeveloper/Small-Injection-Bottle/blob/main/Front%20View.jpg?raw=true";
+const Work7 = "https://github.com/ShavinduDeveloper/Our-Bank/blob/main/Prototype/02.png?raw=true";
+const Work8 = "https://github.com/ShavinduDeveloper/TELL-ME/blob/main/Prototype%20Images/Application/Home.png?raw=true";
+const Work9 = "https://github.com/ShavinduDeveloper/Jar/blob/main/Zoomed%20Jar.jpg?raw=true";
+const Work10 = "https://github.com/ShavinduDeveloper/Tea-Table/blob/main/Zoom%20Side%20View.jpg?raw=true";
+const Work11 = "https://github.com/ShavinduDeveloper/White-Colour-Shirt-Design/blob/main/Shirt%20Design%2001%20%5B%20Mockup%20%5D.jpg?raw=true";
+const Work12 = "https://github.com/ShavinduDeveloper/3D-Village/blob/main/Front%20View.jpg?raw=true";
+const Work13 = "https://github.com/ShavinduDeveloper/Foley-Project-01/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
+const Work14 = "https://github.com/ShavinduDeveloper/Foley-Project-02/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
+const Work15 = "https://github.com/ShavinduDeveloper/Font-Store/blob/main/src/Font%20Store.jpeg?raw=true";
+const Work16 = "https://github.com/ShavinduDeveloper/Web-Application-Calculator/blob/main/06%20Calculator%20Wirefram%20%E2%80%93%20with%20Icon-1.png?raw=true";
+const Work17 = "https://github.com/ShavinduDeveloper/Web-Application-STS-Software-Development/blob/main/STS%20Software%20Development%20Mockup.png?raw=true";
+const Work18 = "";
+const Work19 = "https://github.com/ShavinduDeveloper/Online-Greeting-Cards---Web-Project/blob/main/Resources/img/OnlineGreetingCards.jpeg?raw=true";
+const Work20 = "https://github.com/ShavinduDeveloper/NationalFuelPass/blob/main/src/NationalFuelPass.jpeg?raw=true              ";
+const Work21 = "";
+const Work22 = "";
+const Work23 = "";
+const Work24 = "";
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: "National Fuel Pass ",
-        url: "https://github.com/ShavinduDeveloper/Portfolio-Website",
-        category: "Web",
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: "SchooliGo Mobile Application",
-        url: "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application",
-        category: "Development",
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: "White Colour Shirt Design",
-        url: "https://github.com/ShavinduDeveloper/White-Colour-Shirt-Design",
-        category: "design",
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: "ASTA",
-        url: "https://github.com/ShavinduDeveloper/ASTA",
-        category: "Development",
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: "Online Greeting Cards",
-        url: "https://github.com/ShavinduDeveloper/Online-Greeting-Cards---Web-Project",
-        category: "Web",
-    },
-    {
-        id: 6,
-        image: Work4,
-        title: "3D-Village",
-        url: "https://github.com/ShavinduDeveloper/3D-Village",
-        category: "design",
-    },
-    {
-        id: 7,
-        image: Work5,
         title: "ECOSOL",
         url: "https://github.com/ShavinduDeveloper/ECOSOL",
         category: "design",
     },
     {
-        id: 8,
-        image: Work5,
+        id: 2,
+        image: Work2,
         title: "Sidebar",
         url: "https://github.com/ShavinduDeveloper/Sidebar",
         category: "design",
     },
     {
-        id: 9,
+        id: 3,
+        image: Work3,
+        title: "SchooliGo Mobile Application",
+        url: "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application",
+        category: "design",
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: "Doughnut",
+        url: "https://github.com/ShavinduDeveloper/Doughnut",
+        category: "design",
+    },
+    {
+        id: 5,
         image: Work5,
-        title: "Foley Project 01",
-        url: "https://github.com/ShavinduDeveloper/Foley-Project-01",
-        category: "Foley",
+        title: "7Up Bottle",
+        url: "https://github.com/ShavinduDeveloper/7Up-Bottle",
+        category: "design",
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "Small Injection Bottle",
+        url: "https://github.com/ShavinduDeveloper/Small-Injection-Bottle",
+        category: "design",
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: "Our Bank",
+        url: "https://github.com/ShavinduDeveloper/Our-Bank",
+        category: "design",
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: "TELL ME",
+        url: "https://github.com/ShavinduDeveloper/TELL-ME",
+        category: "design",
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: "Jar",
+        url: "https://github.com/ShavinduDeveloper/Jar",
+        category: "design",
     },
     {
         id: 10,
-        image: Work5,
-        title: "Foley Project 02",
-        url: "https://github.com/ShavinduDeveloper/Foley-Project-02",
-        category: "Foley",
+        image: Work10,
+        title: "Tea Table",
+        url: "https://github.com/ShavinduDeveloper/Tea-Table",
+        category: "design",
     },
     {
         id: 11,
-        image: Work1,
-        title: "Portfolio Website",
-        url: "https://github.com/ShavinduDeveloper/Portfolio-Website",
-        category: "Web",
+        image: Work11,
+        title: "White Colour Shirt Design",
+        url: "https://github.com/ShavinduDeveloper/White-Colour-Shirt-Design",
+        category: "design",
     },
     {
         id: 12,
-        image: Work1,
+        image: Work12,
+        title: "3D Village",
+        url: "https://github.com/ShavinduDeveloper/3D-Village",
+        category: "design",
+    },
+    {
+        id: 13,
+        image: Work13,
+        title: "Foley Project 01",
+        url: "https://github.com/ShavinduDeveloper/Foley-Project-01",
+        category: "foley",
+    },
+    {
+        id: 14,
+        image: Work14,
+        title: "Foley Project 02",
+        url: "https://github.com/ShavinduDeveloper/Foley-Project-02",
+        category: "foley",
+    },
+    {
+        id: 15,
+        image: Work15,
         title: "Font Store",
         url: "https://github.com/ShavinduDeveloper/Font-Store",
         category: "Web",
     },
     {
-        id: 13,
-        image: Work1,
+        id: 16,
+        image: Work16,
         title: "Calculator",
         url: "https://github.com/ShavinduDeveloper/Web-Application-Calculator",
         category: "Web",
     },
     {
-        id: 14,
-        image: Work1,
+        id: 17,
+        image: Work17,
         title: "STS Software Development",
         url: "https://github.com/ShavinduDeveloper/Web-Application-STS-Software-Development",
         category: "Web",
     },
     {
-        id: 15,
-        image: Work1,
-        title: "TELL-ME",
-        url: "https://github.com/ShavinduDeveloper/TELL-ME",
-        category: "design",
-    },
-    {
-        id: 16,
-        image: Work1,
-        title: "MediHelp",
-        url: "https://github.com/ShavinduDeveloper/MediHelp",
-        category: "Web",
-    },
-    {
-        id: 17,
-        image: Work1,
-        title: "MediHelp",
-        url: "https://github.com/ShavinduDeveloper/MediHelp",
-        category: "Web",
-    },
-    {
         id: 18,
-        image: Work1,
+        image: Work18,
         title: "MediHelp",
         url: "https://github.com/ShavinduDeveloper/MediHelp",
         category: "Web",
     },
     {
         id: 19,
-        image: Work1,
-        title: "MediHelp",
-        url: "https://github.com/ShavinduDeveloper/MediHelp",
+        image: Work19,
+        title: "Online Greeting Cards",
+        url: "https://github.com/ShavinduDeveloper/Online-Greeting-Cards---Web-Project",
         category: "Web",
     },
     {
         id: 20,
-        image: Work1,
-        title: "MediHelp",
-        url: "https://github.com/ShavinduDeveloper/MediHelp",
+        image: Work20,
+        title: "National Fuel Pass",
+        url: "https://github.com/ShavinduDeveloper/NationalFuelPass",
         category: "Web",
+    },    
+    {
+        id: 21,
+        image: Work21,
+        title: "Portfolio Website",
+        url: "https://github.com/ShavinduDeveloper/Portfolio-Website",
+        category: "Web",
+    },
+    {
+        id: 22,
+        image: Work22,
+        title: "SchooliGo Mobile Application",
+        url: "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application",
+        category: "Development",
+    },
+    {
+        id: 23,
+        image: Work23,
+        title: "ASTA",
+        url: "https://github.com/ShavinduDeveloper/ASTA",
+        category: "Development",
+    },
+    {
+        id: 24,
+        image: Work24,
+        title: "Enemy Destroyer",
+        url: "https://github.com/ShavinduDeveloper/Enemy-Destroyer",
+        category: "Development",
     },
 ];
 export const projectsNav =[
@@ -154,7 +201,7 @@ export const projectsNav =[
         name: 'design',
     },
     {
-        name: 'Foley',
+        name: 'foley',
     },
     {
         name: 'Web',
