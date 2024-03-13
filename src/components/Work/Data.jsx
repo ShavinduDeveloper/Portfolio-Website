a053c2eaa084894c0d1cd638d707ef0e802ec47b
Shavindu Thushara <shavindu.thushara.2001.developer@gmail.com>
2024-03-13 14:02:36 +0530

    Update Data.jsx

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -21,7 +21,7 @@ const Work20 = "https://github.com/ShavinduDeveloper/NationalFuelPass/blob/main/
 const Work21 = "https://github.com/ShavinduDeveloper/Portfolio-Website/blob/main/src/assets/Portfolio%20Website%20View.jpeg?raw=true";
 const Work22 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
 const Work23 = "https://github.com/ShavinduDeveloper/ASTA/blob/main/Images/3.png?raw=true";
-const Work24 = "https://github.com/ShavinduDeveloper/Enemy-Destroyer/blob/main/Enemy%20Destroyer%20.png?raw=true";
+const Work24 = "https://github.com/ShavinduDeveloper/Enemy-Destroyer/blob/main/Enemy%20Destroyer%20Post.png?raw=true";
 
 export const projectsData = [
     {
