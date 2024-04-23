e01af19a944f9fd55c36c28b5bbc800864b2c4c5
Shavindu Thushara <shavindu.thushara.2001.developer@gmail.com>
2024-04-23 11:21:32 +0530

    Update Data.jsx

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -1,37 +1,72 @@
-const Work01 = "https://github.com/ShavinduDeveloper/ECOSOL/blob/main/ECOSOL%20Introducing%20Post.png?raw=true";
-const Work02 = "https://github.com/ShavinduDeveloper/Sidebar/blob/main/Introducing%20Sidebar%20App%20Presentation.png?raw=true";
-const Work03 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
-const Work04 = "https://github.com/ShavinduDeveloper/Doughnut/blob/main/Views/Views%20of%20After%20Adding%20Texture%20and%20Colours/Left%20View.jpg?raw=true";
-const Work05 = "https://github.com/ShavinduDeveloper/7Up-Bottle/blob/main/Front%20View.jpg?raw=true";
-const Work06 = "https://github.com/ShavinduDeveloper/Small-Injection-Bottle/blob/main/Front%20View.jpg?raw=true";
-const Work07 = "https://github.com/ShavinduDeveloper/Our-Bank/blob/main/Prototype/02.png?raw=true";
-const Work08 = "https://github.com/ShavinduDeveloper/TELL-ME/blob/main/Prototype%20Images/Application/Home.png?raw=true";
-const Work09 = "https://github.com/ShavinduDeveloper/Jar/blob/main/Zoomed%20Jar.jpg?raw=true";
-const Work10 = "https://github.com/ShavinduDeveloper/Tea-Table/blob/main/Zoom%20Side%20View.jpg?raw=true";
-const Work11 = "https://github.com/ShavinduDeveloper/White-Colour-Shirt-Design/blob/main/Shirt%20Design%2001%20%5B%20Mockup%20%5D.jpg?raw=true";
-const Work12 = "https://github.com/ShavinduDeveloper/3D-Village/blob/main/Front%20View.jpg?raw=true";
-const Work13 = "https://github.com/ShavinduDeveloper/Ai/blob/main/Prototype/Business%20Card%20on%20Macbook%20Pro%20Mockup.png?raw=true";
-const Work14 = "https://github.com/ShavinduDeveloper/STS/blob/main/Prototype/Rubber%20Stamp%20%20MockUp%202.png?raw=true";
-const Work15 = "https://github.com/ShavinduDeveloper/STS-AV/blob/main/Prototype/Artwork%20in%20wall.png?raw=true";
-const Work16 = "https://github.com/ShavinduDeveloper/HR/blob/main/Prototype/Floating%20Tag%20Mockup.png?raw=true";
-const Work17 = "https://github.com/ShavinduDeveloper/UXpress/blob/main/Prototype/Free%20Book%20Mockup%2001.png?raw=true";
-const Work18 = "https://github.com/ShavinduDeveloper/City-Biz-Computers/blob/main/Prototype/transparent%20plate%20mockup.png?raw=true";
-const Work19 = "https://github.com/ShavinduDeveloper/EPIC-Whisper-Creations/blob/main/Prototype/Surface%20Studio%20On%20Table%20Mockup.png?raw=true";
-const Work20 = "https://github.com/ShavinduDeveloper/Foley-Project-01/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
-const Work21 = "https://github.com/ShavinduDeveloper/Foley-Project-02/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
-const Work22 = "https://github.com/ShavinduDeveloper/Font-Store/blob/main/src/Font%20Store.jpeg?raw=true";
-const Work23 = "https://github.com/ShavinduDeveloper/Web-Application-Calculator/blob/main/Calculator%20View.png?raw=true";
-const Work24 = "https://github.com/ShavinduDeveloper/Web-Application-STS-Software-Development/blob/main/STS%20Software%20Development%20Mockup.png?raw=true";
-const Work25 = "https://github.com/ShavinduDeveloper/MediHelp/blob/main/src/img/MediHelp.jpeg?raw=true";
-const Work26 = "https://github.com/ShavinduDeveloper/Online-Greeting-Cards---Web-Project/blob/main/Resources/img/OnlineGreetingCards.jpeg?raw=true";
-const Work27 = "https://github.com/ShavinduDeveloper/NationalFuelPass/blob/main/src/NationalFuelPass.jpeg?raw=true              ";
-const Work28 = "https://github.com/ShavinduDeveloper/Portfolio-Website/blob/main/src/assets/Portfolio%20Website%20View.jpeg?raw=true";
-const Work29 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
-const Work30 = "https://github.com/ShavinduDeveloper/ASTA/blob/main/Images/3.png?raw=true";
-const Work31 = "https://github.com/ShavinduDeveloper/Enemy-Destroyer/blob/main/Enemy%20Destroyer%20Post.png?raw=true";
-const Work32 = "https://github.com/ShavinduDeveloper/Robot-Car/blob/main/Robot%20Car.jpg?raw=true";
-const Work33 = "https://github.com/ShavinduDeveloper/Street-Light/blob/main/Street%20Light.png?raw=true";
-const Work34 = "https://github.com/ShavinduDeveloper/Perfume/blob/main/Views/Perfume%20potfolio.jpg?raw=true";
+import Work01 from "../../assets/Works Images/ECOSOL Introducing Post.png"
+import Work02 from "../../assets/Works Images/Introducing Sidebar App Presentation.png"
+import Work03 from "../../assets/Works Images/SchooliGo Introducing Post.png"
+import Work04 from "../../assets/Works Images/Dougnut Left View.jpg"
+import Work05 from "../../assets/Works Images/7up Bottle Front View.jpg"
+import Work06 from "../../assets/Works Images/Uxpress.jpg"
+import Work07 from "../../assets/Works Images/Our Bank.png"
+import Work08 from "../../assets/Works Images/TellMe.jpg"
+import Work09 from "../../assets/Works Images/Zoomed Jar.jpg"
+import Work10 from "../../assets/Works Images/Zoom Side View.jpg"
+import Work11 from "../../assets/Works Images/Shirt Design 01 [ Mockup ].jpg"
+import Work12 from "../../assets/Works Images/3D village.jpg"
+import Work13 from "../../assets/Works Images/Business Card on Macbook Pro Mockup.png"
+import Work14 from "../../assets/Works Images/Rubber Stamp  MockUp 2.png"
+import Work15 from "../../assets/Works Images/Artwork in wall.png"
+import Work16 from "../../assets/Works Images/Floating Tag Mockup.png"
+import Work17 from "../../assets/Works Images/UxPress Book.png"
+import Work18 from "../../assets/Works Images/transparent plate mockup.png"
+import Work19 from "../../assets/Works Images/Surface Studio On Table Mockup.png"
+import Work20 from "../../assets/Works Images/Foley Project in Fl Studio (1).png"
+import Work21 from "../../assets/Works Images/Foley Project in Fl Studio.png"
+import Work22 from "../../assets/Works Images/Font Store.jpeg"
+import Work23 from "../../assets/Works Images/Calculator View.png"
+import Work24 from "../../assets/Works Images/STS Software Development Mockup.png"
+import Work25 from "../../assets/Works Images/MediHelp.jpeg"
+import Work26 from "../../assets/Works Images/OnlineGreetingCards.jpeg"
+import Work27 from "../../assets/Works Images/NationalFuelPass.jpeg"
+import Work28 from "../../assets/Works Images/Portfolio Website View.jpeg"
+import Work29 from "../../assets/Works Images/SchooliGo Introducing Post.png"
+import Work30 from "../../assets/Works Images/ASTA.png"
+import Work31 from "../../assets/Works Images/Enemy Destroyer Post.png"
+import Work32 from "../../assets/Works Images/Robot Car.jpg"
+import Work33 from "../../assets/Works Images/Street Light.png"
+import Work34 from "../../assets/Works Images/Perfume potfolio.jpg"
+
+// const Work01 = "https://github.com/ShavinduDeveloper/ECOSOL/blob/main/ECOSOL%20Introducing%20Post.png?raw=true";
+// const Work02 = "https://github.com/ShavinduDeveloper/Sidebar/blob/main/Introducing%20Sidebar%20App%20Presentation.png?raw=true";
+// const Work03 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
+// const Work04 = "https://github.com/ShavinduDeveloper/Doughnut/blob/main/Views/Views%20of%20After%20Adding%20Texture%20and%20Colours/Left%20View.jpg?raw=true";
+// const Work05 = "https://github.com/ShavinduDeveloper/7Up-Bottle/blob/main/Front%20View.jpg?raw=true";
+// const Work06 = "https://github.com/ShavinduDeveloper/Small-Injection-Bottle/blob/main/Front%20View.jpg?raw=true";
+// const Work07 = "https://github.com/ShavinduDeveloper/Our-Bank/blob/main/Prototype/02.png?raw=true";
+// const Work08 = "https://github.com/ShavinduDeveloper/TELL-ME/blob/main/Prototype%20Images/Application/Home.png?raw=true";
+// const Work09 = "https://github.com/ShavinduDeveloper/Jar/blob/main/Zoomed%20Jar.jpg?raw=true";
+// const Work10 = "https://github.com/ShavinduDeveloper/Tea-Table/blob/main/Zoom%20Side%20View.jpg?raw=true";
+// const Work11 = "https://github.com/ShavinduDeveloper/White-Colour-Shirt-Design/blob/main/Shirt%20Design%2001%20%5B%20Mockup%20%5D.jpg?raw=true";
+// const Work12 = "https://github.com/ShavinduDeveloper/3D-Village/blob/main/Front%20View.jpg?raw=true";
+// const Work13 = "https://github.com/ShavinduDeveloper/Ai/blob/main/Prototype/Business%20Card%20on%20Macbook%20Pro%20Mockup.png?raw=true";
+// const Work14 = "https://github.com/ShavinduDeveloper/STS/blob/main/Prototype/Rubber%20Stamp%20%20MockUp%202.png?raw=true";
+// const Work15 = "https://github.com/ShavinduDeveloper/STS-AV/blob/main/Prototype/Artwork%20in%20wall.png?raw=true";
+// const Work16 = "https://github.com/ShavinduDeveloper/HR/blob/main/Prototype/Floating%20Tag%20Mockup.png?raw=true";
+// const Work17 = "https://github.com/ShavinduDeveloper/UXpress/blob/main/Prototype/Free%20Book%20Mockup%2001.png?raw=true";
+// const Work18 = "https://github.com/ShavinduDeveloper/City-Biz-Computers/blob/main/Prototype/transparent%20plate%20mockup.png?raw=true";
+// const Work19 = "https://github.com/ShavinduDeveloper/EPIC-Whisper-Creations/blob/main/Prototype/Surface%20Studio%20On%20Table%20Mockup.png?raw=true";
+// const Work20 = "https://github.com/ShavinduDeveloper/Foley-Project-01/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
+// const Work21 = "https://github.com/ShavinduDeveloper/Foley-Project-02/blob/main/Final%20Out/Foley%20Project%20in%20Fl%20Studio.png?raw=true";
+// const Work22 = "https://github.com/ShavinduDeveloper/Font-Store/blob/main/src/Font%20Store.jpeg?raw=true";
+// const Work23 = "https://github.com/ShavinduDeveloper/Web-Application-Calculator/blob/main/Calculator%20View.png?raw=true";
+// const Work24 = "https://github.com/ShavinduDeveloper/Web-Application-STS-Software-Development/blob/main/STS%20Software%20Development%20Mockup.png?raw=true";
+// const Work25 = "https://github.com/ShavinduDeveloper/MediHelp/blob/main/src/img/MediHelp.jpeg?raw=true";
+// const Work26 = "https://github.com/ShavinduDeveloper/Online-Greeting-Cards---Web-Project/blob/main/Resources/img/OnlineGreetingCards.jpeg?raw=true";
+// const Work27 = "https://github.com/ShavinduDeveloper/NationalFuelPass/blob/main/src/NationalFuelPass.jpeg?raw=true              ";
+// const Work28 = "https://github.com/ShavinduDeveloper/Portfolio-Website/blob/main/src/assets/Portfolio%20Website%20View.jpeg?raw=true";
+// const Work29 = "https://github.com/ShavinduDeveloper/SchooliGo-Mobile-Application/blob/main/SchooliGo%20Mobile%20Application/Resources/SchooliGo%20Introducing%20Post.png?raw=true";
+// const Work30 = "https://github.com/ShavinduDeveloper/ASTA/blob/main/Images/3.png?raw=true";
+// const Work31 = "https://github.com/ShavinduDeveloper/Enemy-Destroyer/blob/main/Enemy%20Destroyer%20Post.png?raw=true";
+// const Work32 = "https://github.com/ShavinduDeveloper/Robot-Car/blob/main/Robot%20Car.jpg?raw=true";
+// const Work33 = "https://github.com/ShavinduDeveloper/Street-Light/blob/main/Street%20Light.png?raw=true";
+// const Work34 = "https://github.com/ShavinduDeveloper/Perfume/blob/main/Views/Perfume%20potfolio.jpg?raw=true";
 
 export const projectsData = [
     {
